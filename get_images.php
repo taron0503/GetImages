c6a69261e66e91532b928415106dd46f71ff45e2
Taron <taron0503@gmail.com>
2017-07-14 20:50:17 +0400

    Images

---
+++ get_images.php
@@ -0,0 +1,25 @@
+<?php
+
+function get_images($path){
+	$files = scandir($path);
+	
+	foreach ($files as $key => $value) {
+		
+		if($value != '.' && $value != '..'){
+			if(is_dir($path.'/'.$value)){
+				get_images($path.'/'.$value);
+			}else{
+				$format = pathinfo($path.'/'.$value,PATHINFO_EXTENSION);
+				if(in_array($format,['jpg','png','png']))
+					echo "<img width = 200px height = 200px src = '$path/$value'>";
+			}
+
+		}
+	}
+
+}
+
+get_images('example');
+
+
+?>
